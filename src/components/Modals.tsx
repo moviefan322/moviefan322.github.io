74808d22296768d503d1e1662f2b381a85bff5af
moviefan322 <philman202@yahoo.com>
2023-09-03 12:31:20 -0400

    adds modal content for projects

--- src/components/Modals.tsx
+++ src/components/Modals.tsx
@@ -7,15 +7,38 @@ function Modals() {
           <div className="modal-content bg-secondary p-5">
             <div className="row">
               <div className="col-md-6">
-                <img src="images/project-1.jpg" alt="" className="img-fluid" />
+                <img
+                  src="mlb-app2.png"
+                  alt=""
+                  className="img-fluid border border-dark"
+                />
               </div>
-              <div className="col-md-6">
-                <h3>Orkla Foods</h3>
-
-                <a href="#" className="btn btn-primary text-white"> Details </a>
-                <button className="btn btn-light" data-bs-dismiss="modal">
-                  Close
-                </button>
+              <div className="col-md-6 mt-4 text-center">
+                <h3>AllSports</h3>
+                <p>
+                  AllSports is built on a classic MERN stack with Redux. The
+                  application allows users to create a profile, follow their
+                  favorite sports teams, and bet on live games.
+                </p>
+                <div className="mt-7 d-flex justify-content-between">
+                  <a
+                    href="https://sports-app322.herokuapp.com/"
+                    className="btn btn-primary text-white btn-outline-dark"
+                  >
+                    {" "}
+                    Site{" "}
+                  </a>
+                  <a
+                    href="https://github.com/moviefan322/mlbapi"
+                    className="btn btn-dark text-white mx-4"
+                  >
+                    {" "}
+                    Code{" "}
+                  </a>
+                  <button className="btn btn-light" data-bs-dismiss="modal">
+                    Close
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -27,24 +50,40 @@ function Modals() {
         <div className="modal-dialog modal-lg mt-7">
           <div className="modal-content bg-secondary p-5">
             <div className="row">
-              <div className="col-md-6">
-                <img src="images/project-2.jpg" alt="" className="img-fluid" />
+              <div className="col-md-6 mt-4">
+                <img
+                  src="espan3.png"
+                  alt=""
+                  className="img-fluid border-dark border"
+                />
               </div>
-              <div className="col-md-6">
-                <h3>First Bank</h3>
+              <div className="col-md-6 mt-4 text-center">
+                <h3>Españolified</h3>
                 <p>
-                  Aliquam at accumsan nibh. Praesent pretium ut tortor ac
-                  commodo. Integer semper quam at rutrum interdum. Mauris eu
-                  diam ac urna laoreet mollis et
+                  Españolified is built with a Nest.js server with PostresSQL
+                  and Next.js front-end. It is basically an interactive
+                  Spanish-language text book with lessons, quizzes, and allows
+                  users to track their progress and stats as they learn Spanish.
                 </p>
-                <p>
-                  Praesent lacus tortor, facilisis non enim eu, luctus semper
-                  velit. Curabitur faucibus diam in lectus.
-                </p>
-                <a href="#" className="btn btn-primary text-white"> Details </a>
-                <button className="btn btn-light" data-bs-dismiss="modal">
-                  Close
-                </button>
+                <div className="mt-7 d-flex justify-content-between">
+                  <a
+                    href="https://espanolified.vercel.app/"
+                    className="btn btn-primary text-white btn-outline-dark"
+                  >
+                    {" "}
+                    Site{" "}
+                  </a>
+                  <a
+                    href="https://github.com/moviefan322/spanish-app"
+                    className="btn btn-dark text-white mx-4"
+                  >
+                    {" "}
+                    Code{" "}
+                  </a>
+                  <button className="btn btn-light" data-bs-dismiss="modal">
+                    Close
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -56,24 +95,37 @@ function Modals() {
         <div className="modal-dialog modal-lg mt-7">
           <div className="modal-content bg-secondary p-5">
             <div className="row">
-              <div className="col-md-6">
-                <img src="images/project-3.jpg" alt="" className="img-fluid" />
+              <div className="col-md-6 mt-4">
+                <img src="love-hungry-1.png" alt="" className="img-fluid" />
               </div>
-              <div className="col-md-6">
-                <h3>All Sweets</h3>
+              <div className="col-md-6 mt-4 text-center">
+                <h3>Love Hungry</h3>
                 <p>
-                  Aliquam at accumsan nibh. Praesent pretium ut tortor ac
-                  commodo. Integer semper quam at rutrum interdum. Mauris eu
-                  diam ac urna laoreet mollis et
+                  Love Hungry is built with Node.js and handlebars. It is a
+                  dating application based on the simple question of "What do
+                  you want to eat tonight?". It matches users with dates in
+                  their area interested in eating the same cuisine and
+                  recommends restaurants near you.
                 </p>
-                <p>
-                  Praesent lacus tortor, facilisis non enim eu, luctus semper
-                  velit. Curabitur faucibus diam in lectus.
-                </p>
-                <a href="#" className="btn btn-primary text-white"> Details </a>
-                <button className="btn btn-light" data-bs-dismiss="modal">
-                  Close
-                </button>
+                <div className="mt-7 d-flex justify-content-between">
+                  <a
+                    href="https://love-hungry.herokuapp.com/"
+                    className="btn btn-primary text-white btn-outline-dark"
+                  >
+                    {" "}
+                    Site{" "}
+                  </a>
+                  <a
+                    href="https://github.com/moviefan322/love-hungry"
+                    className="btn btn-dark text-white mx-4"
+                  >
+                    {" "}
+                    Code{" "}
+                  </a>
+                  <button className="btn btn-light" data-bs-dismiss="modal">
+                    Close
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -85,24 +137,37 @@ function Modals() {
         <div className="modal-dialog modal-lg mt-7">
           <div className="modal-content bg-secondary p-5">
             <div className="row">
-              <div className="col-md-6">
-                <img src="images/project-4.jpg" alt="" className="img-fluid" />
+              <div className="col-md-6 mt-4">
+                <img
+                  src="laronix.png"
+                  alt=""
+                  className="img-fluid"
+                  style={{ height: "400px", marginLeft: "20%" }}
+                />
               </div>
-              <div className="col-md-6">
-                <h3>Fast Courier</h3>
+              <div className="col-md-6 mt-4 text-center">
+                <h3>Laronix Mobile</h3>
                 <p>
-                  Aliquam at accumsan nibh. Praesent pretium ut tortor ac
-                  commodo. Integer semper quam at rutrum interdum. Mauris eu
-                  diam ac urna laoreet mollis et
+                  Laronix Mobile is built with a Next.js front end and a
+                  python/hugging face/gradio back-end. The application allows
+                  users to record an audio clip of speech and transform it into
+                  different voices. The target users are laryngectomy patients
+                  communicating through voice-boxes. The code for this
+                  application is proprietary and not available for public
+                  viewing.
                 </p>
-                <p>
-                  Praesent lacus tortor, facilisis non enim eu, luctus semper
-                  velit. Curabitur faucibus diam in lectus.
-                </p>
-                <a href="#" className="btn btn-primary text-white"> Details </a>
-                <button className="btn btn-light" data-bs-dismiss="modal">
-                  Close
-                </button>
+                <div className="mt-8 d-flex justify-content-between">
+                  <a
+                    href="https://develop.d3igioyq2kg6x4.amplifyapp.com/"
+                    className="btn btn-primary text-white btn-outline-dark"
+                  >
+                    {" "}
+                    Site{" "}
+                  </a>
+                  <button className="btn btn-light" data-bs-dismiss="modal">
+                    Close
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -114,24 +179,36 @@ function Modals() {
         <div className="modal-dialog modal-lg mt-7">
           <div className="modal-content bg-secondary p-5">
             <div className="row">
-              <div className="col-md-6">
-                <img src="images/project-5.jpg" alt="" className="img-fluid" />
+              <div className="col-md-6 mt-4">
+                <img
+                  src="ifLogoMini.png"
+                  alt=""
+                  className="img-fluid bg-light p-5 position-relative w-75 top-50 start-50 translate-middle"
+                />
               </div>
-              <div className="col-md-6">
-                <h3>Lyra Market</h3>
+              <div className="col-md-6 mt-4 text-center">
+                <h3>Invisible Friend</h3>
                 <p>
-                  Aliquam at accumsan nibh. Praesent pretium ut tortor ac
-                  commodo. Integer semper quam at rutrum interdum. Mauris eu
-                  diam ac urna laoreet mollis et
+                  Invisible Friend is built with a classic MERN stack and
+                  GraphQL. It is a chatbot that you can customize and talk to,
+                  the bot will remember everything you have talked about before
+                  and always remain your loyal friend. Unfortunately, this
+                  application was too costly to host and is no longer available
+                  online, but the code is available for viewing and it may be
+                  run locally.
                 </p>
-                <p>
-                  Praesent lacus tortor, facilisis non enim eu, luctus semper
-                  velit. Curabitur faucibus diam in lectus.
-                </p>
-                <a href="#" className="btn btn-primary text-white"> Details </a>
-                <button className="btn btn-light" data-bs-dismiss="modal">
-                  Close
-                </button>
+                <div className="mt-7 d-flex justify-content-between">
+                  <a
+                    href="https://github.com/moviefan322/Invisible-Friend"
+                    className="btn btn-dark text-white btn-outline-dark"
+                  >
+                    {" "}
+                    Code{" "}
+                  </a>
+                  <button className="btn btn-light" data-bs-dismiss="modal">
+                    Close
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -143,31 +220,41 @@ function Modals() {
         <div className="modal-dialog modal-lg mt-7">
           <div className="modal-content bg-secondary p-5">
             <div className="row">
-              <div className="col-md-6">
-                <img src="images/project-6.jpg" alt="" className="img-fluid" />
+              <div className="col-md-6 mt-4">
+                <img src="devblog.png" alt="" className="img-fluid" />
               </div>
-              <div className="col-md-6">
-                <h3>Monday Stores</h3>
+              <div className="col-md-6 mt-4 text-center">
+                <h3>Dev Blog</h3>
                 <p>
-                  Aliquam at accumsan nibh. Praesent pretium ut tortor ac
-                  commodo. Integer semper quam at rutrum interdum. Mauris eu
-                  diam ac urna laoreet mollis et
+                  This is my dev blog where I write about my coding adventures.
+                  It is built entirely on Next.js.
                 </p>
-                <p>
-                  Praesent lacus tortor, facilisis non enim eu, luctus semper
-                  velit. Curabitur faucibus diam in lectus.
-                </p>
-                <a href="#" className="btn btn-primary text-white"> Details </a>
-                <button className="btn btn-light" data-bs-dismiss="modal">
-                  Close
-                </button>
+                <div className="mt-7 d-flex justify-content-between">
+                  <a
+                    href="https://next-ts-blog-lake.vercel.app/"
+                    className="btn btn-primary text-white btn-outline-dark"
+                  >
+                    {" "}
+                    Site{" "}
+                  </a>
+                  <a
+                    href="https://github.com/moviefan322/next-ts-blog"
+                    className="btn btn-dark text-white mx-4"
+                  >
+                    {" "}
+                    Code{" "}
+                  </a>
+                  <button className="btn btn-light" data-bs-dismiss="modal">
+                    Close
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Modals
+export default Modals;
