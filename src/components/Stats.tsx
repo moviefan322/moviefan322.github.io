4012a6d46ef2d6bd8b83f9727b2c6346ab516298
moviefan322 <philman202@yahoo.com>
2023-09-04 23:19:18 -0400

    removes pel from stats

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -16,7 +16,6 @@ export default function Stats() {
             <div className="stat text-center border-end border-dark">
               <FaUsers className="h1" />
               <p className="fs-1">500+</p>
-              <h3 className="fs-6 text-uppercase">Clients</h3>
             </div>
           </div>
 
@@ -24,7 +23,6 @@ export default function Stats() {
             <div className="stat text-center border-end border-dark">
               <FaProjectDiagram className="h1" />
               <p className="fs-1">700+</p>
-              <h3 className="fs-6 text-uppercase">Projects</h3>
             </div>
           </div>
 
@@ -32,7 +30,6 @@ export default function Stats() {
             <div className="stat text-center border-end border-dark">
               <FaClock className="h1" />
               <p className="fs-1">16+</p>
-              <h3 className="fs-6 text-uppercase">Years</h3>
             </div>
           </div>
 
@@ -40,7 +37,6 @@ export default function Stats() {
             <div className="stat text-center border-end border-dark">
               <FaBook className="h1" />
               <p className="fs-1">25</p>
-              <h3 className="fs-6 text-uppercase">Publications</h3>
             </div>
           </div>
 
@@ -48,7 +44,6 @@ export default function Stats() {
             <div className="stat text-center border-end border-dark">
               <FaYoutube className="h1" />
               <p className="fs-1">5K+</p>
-              <h3 className="fs-6 text-uppercase">Subscribers</h3>
             </div>
           </div>
 
@@ -56,7 +51,6 @@ export default function Stats() {
             <div className="stat text-center border-end border-dark">
               <FaCode className="h1" />
               <p className="fs-1">10M+</p>
-              <h3 className="fs-6 text-uppercase">Lines of Code</h3>
             </div>
           </div>
         </div>
