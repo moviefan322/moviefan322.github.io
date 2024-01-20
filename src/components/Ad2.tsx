4f3416becbf1883739a1ca817143dab9306e5427
moviefan322 <philman202@yahoo.com>
2024-01-20 18:23:53 -0500

    converts png to jpg

--- src/components/Ad2.tsx
+++ src/components/Ad2.tsx
@@ -5,9 +5,7 @@ function Ad1() {
     <section className="bg-primary py-4 ad1">
       <div className="container">
         <div className="row pt-3 adrow">
-          <div className="col-md-4 mb-4 d-none d-md-block">
-            <img src="nat2.png" alt="" height="250px" />
-          </div>
+          <div className="col-md-4 mb-4 d-none d-md-block"></div>
           <div className="col-md-4 text-left ms-md-7 ms-6 col-8">
             <div className="row">
               <div className="col-md-12">
@@ -17,15 +15,18 @@ function Ad1() {
             <div className="row perrow">
               <h2 className="text-white">
                 {" "}
-                <FaCheck />{" "}
-                PLAN
+                <FaCheck /> PLAN
               </h2>
             </div>
             <div className="row perrow">
-              <h2 className="text-primary"><FaCheck />{" "}EXECUTE</h2>
+              <h2 className="text-primary">
+                <FaCheck /> EXECUTE
+              </h2>
             </div>
             <div className="row perrow">
-              <h2 className="text-white"><FaCheck />{" "}REPEAT</h2>
+              <h2 className="text-white">
+                <FaCheck /> REPEAT
+              </h2>
             </div>
           </div>
         </div>
