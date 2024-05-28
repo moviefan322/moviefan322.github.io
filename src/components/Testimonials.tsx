2a18cd48364ece3705e2c0201759329b7ffa60ca
moviefan322 <philman202@yahoo.com>
2024-05-28 13:56:22 -0400

    adds testimonial 3

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -8,16 +8,12 @@ function Testimonials() {
           {/* <!-- Slide 1 --> */}
           <div className="carousel-item active">
             <div className="d-flex flex-column justify-content-center align-items-center text-center">
-              <img
-                src="hari.png"
-                width="120px"
-                className="rounded-circle"
-              />
+              <img src="hari.png" width="120px" className="rounded-circle" />
               <p className="w-50 my-4 fst-italic fs-4 mb-4">
-                "Phil is a{" "}
-                <span className="text-primary"> varacious</span> and 
-                <span className="text-primary"> capable</span> engineer. He's been an absolute{" "}
-                <span className="text-primary"> asset</span> for our team."
+                "Phil is a <span className="text-primary"> varacious</span> and
+                <span className="text-primary"> capable</span> engineer. He's
+                been an absolute <span className="text-primary"> asset</span>{" "}
+                for our team."
               </p>
               <div className="fw-bold fs-5 mt-4">Sriharo Rao</div>
               <div>Founder - King Henry</div>
@@ -26,16 +22,13 @@ function Testimonials() {
           {/* <!-- Slide 2 --> */}
           <div className="carousel-item">
             <div className="d-flex flex-column justify-content-center align-items-center text-center">
-              <img
-                src="hanley.png"
-                width="120px"
-                className="rounded-circle"
-              />
+              <img src="hanley.png" width="120px" className="rounded-circle" />
               <p className="w-50 my-4 fst-italic fs-4 mb-4">
-                "Working with Phil was an absolute <span className="text-primary">pleasure </span>. His{" "}
+                "Working with Phil was an absolute{" "}
+                <span className="text-primary">pleasure </span>. His{" "}
                 <span className="text-primary">professionalism </span>and{" "}
-                <span className="text-primary"> communication skills</span>{" "}
-                made the project incredibly smooth.”
+                <span className="text-primary"> communication skills</span> made
+                the project incredibly smooth.”
               </p>
               <div className="fw-bold fs-5 mt-4">Tom Hanley</div>
               <div>Events & Marketing Manager - Redbank River Center</div>
@@ -44,26 +37,19 @@ function Testimonials() {
           {/* <!-- Slide 3 --> */}
           <div className="carousel-item">
             <div className="d-flex flex-column justify-content-center align-items-center text-center">
-              <img
-                src="testimonial-3.jpg"
-                width="120px"
-                className="rounded-circle"
-              />
+              <img src="kavon.png" width="120px" className="rounded-circle" />
               <p className="w-50 my-4 fst-italic fs-4 mb-4">
-                "Phil's problem-solving skills and adaptability were truly
-                <span className="text-primary"> exceptional</span>. He
-                consistently approached challenges with a
-                <span className="text-primary"> creative</span> and{" "}
-                <span className="text-primary">innovative</span> mindset, coming
-                up with <span className="text-primary"> solutions</span> that
-                exceeded our expectations. His ability to pivot and adapt to
-                changing project requirements showcased his{" "}
-                <span className="text-primary"> versatility</span> and
-                <span className="text-primary"> dedication</span> to our
-                success."
+                "Phil is great to work with. He consistently brings{" "}
+                <span className="text-primary">creative</span> and{" "}
+                <span className="text-primary">effective</span> solutions. His{" "}
+                <span className="text-primary">adaptability</span> and{" "}
+                <span className="text-primary">dedication</span> make him a
+                valuable team member. Plus, his{" "}
+                <span className="text-primary">positive attitude</span> and
+                clear communication always drive our projects to success."
               </p>
-              <div className="fw-bold fs-5 mt-4">Alex Reynolds</div>
-              <div>Design Chief - QuantumTech Solutions, LLC</div>
+              <div className="fw-bold fs-5 mt-4">Kavon Kavoussi</div>
+              <div>Founder - AI Political Consultants</div>
             </div>
           </div>
         </div>
